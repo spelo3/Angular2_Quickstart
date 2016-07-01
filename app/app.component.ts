072960fdeb1d58a3204c9bd5f8cfae5265d9af36
Scott <spelo3@gmail.com>
2016-07-01 14:29:10 -0700

    Added first component

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `<h1>{{title}}</h1>
+        <h2>{{race.name}}</h2>
+        <p>{{race.date}}</p>
+        <p>{{race.about}}</p>
+        <p>{{race.entryFee}}</p>`
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'Ultra Racing';
+    race = {
+        "id": 1,
+        "name": "Daytona Thunderdome",
+        "date": new Date('2512-01-04T14:00:00'),
+        "about": "Race through the ruins of an ancient Florida battle arena.",
+        "entryFee": 3200
+    };
+}
